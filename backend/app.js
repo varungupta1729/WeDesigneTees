2d9cbf63c92be94aa983dffb7036fa4817f0df27
varungupta1729 <varun.2125cse1032@kiet.edu>
2024-05-31 15:56:03 +0530

    1111

--- backend/app.js
+++ backend/app.js
@@ -13,9 +13,15 @@ dotenv.config();
 app.use(cors({
   origin: ['https://we-designe-tees.vercel.app/'],
   credentials: true,
-  allowedHeaders: ['Content-Type'],
-  exposedHeaders: ['Content-Type']
+
 }));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://we-designe-tees.vercel.app');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  next();
+});
 // app.use(express.json());
 app.use(express.json({ limit: '1000000000mb' }));
 
--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -35,7 +35,7 @@ console.log(isAuthenticated)
         window.location.reload(true);
       }).catch((err)=>{
         console.log(err);
-        toast.error(err.response.data.message);
+        toast.error(err?.response?.data?.message);
       });
   };
 
