e0128d121910e84bd244198f9b699999110fc840
varungupta1729 <varun.2125cse1032@kiet.edu>
2024-05-31 17:53:49 +0530

    hbfuiewbfciuwedwq

--- backend/app.js
+++ backend/app.js
@@ -11,7 +11,7 @@ const path = require("path");
 dotenv.config();
 
 app.use(cors({
-  origini : 'https://we-designe-tees.vercel.app/',
+  origin : 'https://we-designe-tees.vercel.app/',
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
  
