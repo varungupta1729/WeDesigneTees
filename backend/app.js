0e4fb57cc1c985504429e14f40b4168af90e7a22
varungupta1729 <varun.2125cse1032@kiet.edu>
2024-05-31 16:57:00 +0530

    finally

--- backend/app.js
+++ backend/app.js
@@ -11,26 +11,10 @@ const path = require("path");
 dotenv.config();
 
 app.use(cors({
-  origin: ['https://we-designe-tees.vercel.app/'],
+  origin: '*',
   credentials: true,
-
+ 
 }));
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'https://we-designe-tees.vercel.app');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
-  // Handle preflight requests
-  if (req.method === 'OPTIONS') {
-    res.setHeader('Access-Control-Allow-Origin', 'https://we-designe-tees.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.sendStatus(200);
-  } else {
-    next();
-  }
-});
 
 // app.use(express.json());
 app.use(express.json({ limit: '1000000000mb' }));
