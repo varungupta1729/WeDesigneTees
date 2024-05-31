9aed77270d588674fd3d92eb38cc20532d4b0265
varungupta1729 <varun.2125cse1032@kiet.edu>
2024-05-31 20:17:01 +0530

    efrnf

--- backend/app.js
+++ backend/app.js
@@ -11,19 +11,14 @@ const path = require("path");
 dotenv.config();
 
 
-// app.use((req, res, next) => {
-//   res.header("Access-Control-Allow-Origin", "https://we-designe-tees.vercel.app/")
-//   next()
-// })
-
 app.use(
   cors({
-    origin: 'https://we-designe-tees.vercel.app/',
-    methods: "GET, POST, PATCH, DELETE, PUT",
-    credentials: true,
+    origin: 'https://we-designe-tees.vercel.app/', // use your actual domain name (or localhost), using * is not recommended
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
+    credentials: true
   })
 )
-
 
 
 // app.use(express.json());
