992a3f9d8f12d19ed0b64134d78e067f9afa93c7
varungupta1729 <varun.2125cse1032@kiet.edu>
2024-05-31 20:20:21 +0530

    efrnf

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,7 @@ dotenv.config();
 
 app.use(
   cors({
-    origin: 'https://we-designe-tees.vercel.app/', // use your actual domain name (or localhost), using * is not recommended
+    origin: 'https://we-designe-tees.vercel.app', // use your actual domain name (or localhost), using * is not recommended
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'HEAD', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Origin', 'X-Requested-With', 'Accept', 'x-client-key', 'x-client-token', 'x-client-secret', 'Authorization'],
     credentials: true
