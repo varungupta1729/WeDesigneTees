c9a9ed3f37533f776f22361aa555c140aaa46100
varungupta1729 <varun.2125cse1032@kiet.edu>
2024-05-31 16:11:59 +0530

    new

--- backend/app.js
+++ backend/app.js
@@ -20,8 +20,18 @@ app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
-  next();
+  // Handle preflight requests
+  if (req.method === 'OPTIONS') {
+    res.setHeader('Access-Control-Allow-Origin', 'https://we-designe-tees.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    res.sendStatus(200);
+  } else {
+    next();
+  }
 });
+
 // app.use(express.json());
 app.use(express.json({ limit: '1000000000mb' }));
 
