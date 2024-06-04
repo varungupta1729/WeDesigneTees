87eef4004291a74649f4b37f70a90aba03c5443b
varungupta1729 <varun.2125cse1032@kiet.edu>
2024-06-05 01:26:55 +0530

    added categories

--- frontend/src/components/Route/FeatureProduct.jsx
+++ frontend/src/components/Route/FeatureProduct.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 // import { productData as data} from '../../static/data';
 import ProductCard from "./ProductCard/ProductCard";
 import { useSelector } from "react-redux";
@@ -10,8 +10,86 @@ import { RxCross2 } from "react-icons/rx";
 const FeatureProduct = () => {
   const [active, setActive] = useState(false);
   const { allProducts } = useSelector((state) => state.products);
+  const [activeProducts, setActiveProducts] = useState(allProducts);
+  const [loading , setLoading] = useState(false);
+  
 
-  console.log(allProducts);
+  useEffect(()=>{
+    setActiveProducts(allProducts);
+  },[allProducts])
+  
+
+  const transferData = (elements) => {
+    
+   
+
+    setActiveProducts(elements);
+  };
+
+  const priceFilter = (e) => {
+    
+    if (e.target.checked) {
+      var num = e.target.parentElement.textContent.replace(/[^0-9]/g, "");
+      if (num.length > 3) {
+        let index = num.length - 4;
+        let part1 = num.substring(0, index);
+        let part2 = num.substring(index);
+        
+        transferData(
+          allProducts.filter(
+            (item) => item?.discountPrice < part2 && item?.discountPrice > part1
+          )
+        );
+      } else {
+      
+        transferData(
+          allProducts.filter((item) => item?.discountPrice < num)
+        );
+      }
+    } else {
+     console.log("jbeud");
+      transferData(allProducts);
+    }
+  };
+
+  let checkboxes = document.querySelectorAll('.checkbox');
+  checkboxes.forEach(function(checkbox) {
+   
+      checkbox.addEventListener('change', function() {
+          if (this.checked) {
+              // Uncheck all other checkboxes
+             
+              checkboxes.forEach(function(otherCheckbox) {
+                  if (otherCheckbox !== checkbox) {
+                      otherCheckbox.checked = false;
+                  }
+              });
+          }
+      });
+  });
+
+  
+  const categoryFilter = (e) => {
+    
+    if (e.target.checked) {
+      var categoryClicked = e.target.parentElement.textContent;
+     
+      
+        transferData(
+          allProducts.filter(
+            (item) => item.category === categoryClicked
+          )
+        );
+    
+    } else {
+     
+      transferData(allProducts);
+    }
+  };
+
+
+console.log(activeProducts)
+
   return (
     <div className="w-full  ">
       <div className="text-3xl py-7 text-center font-medium">
@@ -20,10 +98,14 @@ const FeatureProduct = () => {
       <div className="w-full relative  flex justify-center  ">
         {active && (
           <div className="w-[25%] h-[100%] height-div rounded-[20px] mx-10 side-div flex flex-col gap-7 z-10 py-10 px-14 bg-white">
-
             <div className="w-full ">
               <div className="w-full flex flex-col gap-3">
-              <div className="cross justify-end hidden" onClick={()=>setActive(!active)}><RxCross2 size={22}/></div>
+                <div
+                  className="cross justify-end hidden"
+                  onClick={() => setActive(!active)}
+                >
+                  <RxCross2 size={22} />
+                </div>
                 <h2 className="font-semibold text-lg">Categories</h2>
 
                 {categoriesData &&
@@ -31,7 +113,12 @@ const FeatureProduct = () => {
                     return (
                       <div className="w-full">
                         <label class="custom-checkbox w-full">
-                          <input type="checkbox" />
+                          <input
+                            className="checkbox"
+                            type="checkbox"
+                            name="checkbox"
+                            onChange={(e)=>categoryFilter(e)}
+                          />
                           <span class="checkmark"></span>
                           {category.title}
                         </label>
@@ -47,29 +134,36 @@ const FeatureProduct = () => {
 
                 <div className="w-full">
                   <label class="custom-checkbox w-full">
-                    <input type="checkbox" />
+                    <input
+                      className="checkbox"
+                      name="check"
+                      type="checkbox"
+                      onChange={(e) => priceFilter(e)}
+                    />
                     <span class="checkmark"></span>
                     Under INR 999
                   </label>
                 </div>
-              
+
                 <div className="w-full">
                   <label class="custom-checkbox w-full">
-                    <input type="checkbox" />
-                    <span class="checkmark"></span>
-                    INR 999
-                  </label>
-                </div>
-                <div className="w-full">
-                  <label class="custom-checkbox w-full">
-                    <input type="checkbox" />
+                    <input
+                      className="checkbox"
+                      name="check"
+                      type="checkbox"
+                      onChange={(e) => priceFilter(e)}
+                    />
                     <span class="checkmark"></span>
                     INR 999 - INR 1,499
                   </label>
                 </div>
                 <div className="w-full">
                   <label class="custom-checkbox w-full">
-                    <input type="checkbox" />
+                    <input
+                      className="checkbox"
+                      type="checkbox"
+                      onChange={(e) => priceFilter(e)}
+                    />
                     <span class="checkmark"></span>
                     INR 1,499 - INR 1,999
                   </label>
@@ -93,11 +187,13 @@ const FeatureProduct = () => {
             </button>
           </div>
           <div className="  flex flex-wrap justify-center items-center">
-            {allProducts &&
-              allProducts.length !== 0 &&
-              allProducts.map((i, index) => {
+
+            {activeProducts &&
+              activeProducts.length !== 0 &&
+              activeProducts.map((i, index) => {
                 return <ProductCard data={i} key={index} />;
               })}
+          
           </div>
         </div>
       </div>
