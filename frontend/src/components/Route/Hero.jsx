6c6fbb35a342577c6d765a2e5fd23baa5072e25b
varungupta1729 <varun.2125cse1032@kiet.edu>
2024-06-05 02:20:02 +0530

    upadte favicon

--- frontend/src/components/Route/Hero.jsx
+++ frontend/src/components/Route/Hero.jsx
@@ -8,61 +8,71 @@ import {
 const Hero = () => {
   return (
     <>
-      <div className=" h-[70vh]  hidden lg:block">
-        <Carousel>
-          <div
-            className="flex h-full items-center justify-center bg-red-400 dark:bg-gray-700 dark:text-white bg-no-repeat bg-cover"
-            style={{
-              backgroundImage:
-                "url(https://res.cloudinary.com/decvwqui4/image/upload/v1717189833/gpfgndrtrcrofr8iju2y.png)",
-            }}
-          ></div>
+    <div className=" h-[70vh]  hidden lg:block">
+      <Carousel>
 
-          <div
-            className="flex h-full items-center justify-center bg-gradient-to-r bg-black text-black bg-no-repeat bg-cover relative  "
-            style={{
-              backgroundImage:
-                "url(https://res.cloudinary.com/decvwqui4/image/upload/v1717466405/rnbsjlkvma8sssvozmzt.jpg)",
-            }}
-          ></div>
 
-          <div
-            className="flex h-full items-center justify-center bg-red-400 dark:bg-gray-700 dark:text-white bg-no-repeat bg-cover"
-            style={{
-              backgroundImage:
-                "url(https://res.cloudinary.com/decvwqui4/image/upload/v1717466194/fxvn7wag5jtqspdzwpoa.png)",
-            }}
-          ></div>
-        </Carousel>
+      <div className="flex h-full items-center justify-center bg-no-repeat bg-cover"  style={{
+        backgroundImage:
+          "url(https://res.cloudinary.com/decvwqui4/image/upload/v1717189833/gpfgndrtrcrofr8iju2y.png)",
+      }}>
+      
       </div>
 
-      <div className=" h-[30vh]   lg:hidden">
-        <Carousel>
-          <div
-            className="flex h-full items-center justify-center bg-red-400 dark:bg-gray-700 dark:text-white bg-no-repeat bg-cover"
-            style={{
-              backgroundImage:
-                "url(https://res.cloudinary.com/decvwqui4/image/upload/v1717189833/gpfgndrtrcrofr8iju2y.png)",
-            }}
-          ></div>
 
-          <div
-            className="flex h-full items-center justify-center bg-gradient-to-r bg-black text-black bg-no-repeat bg-cover relative  "
-            style={{
-              backgroundImage:
-                "url(https://res.cloudinary.com/decvwqui4/image/upload/v1717466405/rnbsjlkvma8sssvozmzt.jpg)",
-            }}
-          ></div>
 
-          <div
-            className="flex h-full items-center justify-center bg-red-400 dark:bg-gray-700 dark:text-white bg-no-repeat bg-cover"
-            style={{
-              backgroundImage:
-                "url(https://res.cloudinary.com/decvwqui4/image/upload/v1717466194/fxvn7wag5jtqspdzwpoa.png)",
-            }}
-          ></div>
-        </Carousel>
+        <div className="flex h-full items-center justify-center bg-gradient-to-r bg-black text-black bg-no-repeat bg-cover relative  " 
+         style={{
+        backgroundImage:
+          "url(https://res.cloudinary.com/decvwqui4/image/upload/v1717466405/rnbsjlkvma8sssvozmzt.jpg)",}}
+         >
+
+     
       </div>
+
+        <div className="flex h-full items-center justify-cente  dark:bg-gray-700 dark:text-white bg-no-repeat bg-cover"  style={{
+        backgroundImage:
+          "url(https://res.cloudinary.com/decvwqui4/image/upload/v1717466194/fxvn7wag5jtqspdzwpoa.png)",
+      }}>
+      
+      </div>
+
+      
+     
+      </Carousel>
+    </div>
+
+
+    <div className=" h-[30vh]   lg:hidden">
+      <Carousel>
+
+
+      <div className="flex h-full items-center justify-center  dark:text-white bg-no-repeat bg-cover"  style={{
+        backgroundImage:
+          "url(https://res.cloudinary.com/decvwqui4/image/upload/v1717189833/gpfgndrtrcrofr8iju2y.png)",
+      }}>
+      
+      </div>
+
+
+      <div className="flex h-full items-center justify-center bg-gradient-to-r bg-black text-black bg-no-repeat bg-cover relative  " 
+         style={{
+        backgroundImage:
+          "url(https://res.cloudinary.com/decvwqui4/image/upload/v1717466405/rnbsjlkvma8sssvozmzt.jpg)",}}
+         >
+
+     
+      </div>
+
+        
+      <div className="flex h-full items-center justify-center  bg-no-repeat bg-cover"  style={{
+        backgroundImage:
+          "url(https://res.cloudinary.com/decvwqui4/image/upload/v1717466194/fxvn7wag5jtqspdzwpoa.png)",
+      }}>
+      
+      </div>
+      </Carousel>
+    </div>
     </>
   );
 };
