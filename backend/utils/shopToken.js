2913cee52e676c5113b6ee2eaf66cabb6c4d851f
varungupta1729 <varun.2125cse1032@kiet.edu>
2024-06-04 13:54:03 +0530

    change samsite

--- backend/utils/shopToken.js
+++ backend/utils/shopToken.js
@@ -6,8 +6,8 @@ const sendShopToken = (user, statusCode, res) => {
     const options = {
       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      sameSite: "none",
-      secure: true,
+      // sameSite: "none",
+      // secure: true,
     };
   
     res.status(statusCode).cookie("seller_token", token, options).json({
