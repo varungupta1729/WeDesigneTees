38cdf2a365468479f1bc2a6de93eff9e75464a35
varungupta1729 <varun.2125cse1032@kiet.edu>
2024-06-04 14:03:50 +0530

    added more orgin for sending cookie

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -6,10 +6,10 @@ const sendToken = (user, statusCode, res) => {
     const options = {
       expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
       httpOnly: true,
-      // sameSite: "none",
-      // secure: true,
+      sameSite: "None",
+      secure: true,
     };
- -
+ 
     res.status(statusCode).cookie("token", token, options).json({
       success: true,
       user,
